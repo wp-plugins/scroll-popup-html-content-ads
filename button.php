b59b79b5264b8123d973c96488a53697fe13fc6e
gopi_plus <gopi_plus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-11 16:47:17 +0000

    git-svn-id: https://plugins.svn.wordpress.org/scroll-popup-html-content-ads/trunk@542956 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- button.php
+++
@@ -1,4 +0,0 @@
-<div style="float:right;">
-  <input name="text_management" lang="text_management" class="button-primary" onClick="location.href='options-general.php?page=scroll-popup-html-content-ads/content-management.php'" value="Go to - Content Management" type="button" />
-  <input name="setting_management" lang="setting_management" class="button-primary" onClick="location.href='options-general.php?page=scroll-popup-html-content-ads/scroll-popup-html-content-ads.php'" value="Go to - Popup Setting" type="button" />
-</div>
