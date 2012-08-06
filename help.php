0f12e9ec3e30bc6b1dd87e7c2d4e91043dfcdd7c
gopi_plus <gopi_plus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-06 15:06:25 +0000

    git-svn-id: https://plugins.svn.wordpress.org/scroll-popup-html-content-ads/trunk@582311 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ help.php
@@ -0,0 +1,10 @@
+<h2>Plugin configuration</h2>
+<ul>
+	<li>1. To load the popup into particular page, use the page short code. <a href="http://www.gopiplus.com/work/2012/02/05/scroll-popup-html-content-ads-wordpress-plugin/" target="_blank">Click here</a></li>
+	<li>2. To load the popup into all pages in the website, use the given PHP code. copy the php code and paste it in the footer.php file. watch video for more info. <a href="http://www.gopiplus.com/work/2012/02/05/scroll-popup-html-content-ads-wordpress-plugin/" target="_blank">Click here</a></li>
+	<li>3. Click "Go to - Content Management button" to add/update/delete the popup content</li>
+</ul>
+<h2>Plugin help</h2>
+<ul>
+	<li>Check official website for more information <a href="http://www.gopiplus.com/work/2012/02/05/scroll-popup-html-content-ads-wordpress-plugin/" target="_blank">click here</a></li>
+</ul>
